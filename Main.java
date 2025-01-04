8647552a570fb5ef12c3e21cf4ec30cde62b17c3
eschoepf <eduardo.schoepf@hotmail.com>
2025-01-04 11:03:56 +0100

    Define the credit card payment

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import context.PaymentContext;
+import strategy.CreditCardPayment;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Gets the Singleton instance from the context
         PaymentContext context = PaymentContext.getInstance();
+    
+        // Define the credit card payment strategy.
+        context.setPaymentStrategy(new CreditCardPayment("1234-5678-9012-3456"));
+        context.executePayment(250.75);
     }
 }
